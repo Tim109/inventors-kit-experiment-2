a3fa5198f8d6ae3b13bb2ef8a1abd1980e2f43bd
Timothy M Jones <timothy-m-jones@hotmail.co.uk>
2020-09-17 11:11:28 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let Light = 0
+basic.forever(function () {
+    Light = pins.analogReadPin(AnalogPin.P0)
+    if (Light > 512) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # .
+            . # # # .
+            # # # . .
+            `)
+    }
+})
